2cf6811874eeb7cc1cd1f313fa91a25f4f4f571c
omnius008 <42618636+omnius008@users.noreply.github.com>
2020-03-02 07:17:52 -0500

    d

---
+++ Automobile/Automobile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+// Base class
+namespace chapter6LectureCode
+{
+    public class Automobile
+    {
+
+
+        public string Accelerate()
+        {
+            return "zoom!";
+        }
+        //if you make a method or property private, it will not work when you call TheMethod() in Prigram.cs
+        private string InjectFuel()
+        {
+            return "fueling at pump";
+        }
+
+    }
+
+    // Derived class
+    public class Car : Automobile { }
+
+}
